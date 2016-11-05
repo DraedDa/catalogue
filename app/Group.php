a110658f7b0a3d3c0d8fecb5c82d52185e13db66
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-11-05 23:38:15 +0400

    Let's start the Group module
    Addition to te Group Model and Controller

--- app/Group.php
+++ app/Group.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    protected $fillable = [
+      'name',
+      'description'
+    ];
+
     // Add relationship with projets to list all the project lead by a specific group getProjects()
     // Add relationship with users to list all the users whose belong to a specific group getUsers()
     // Add a function to get the owner of a specific group getOwner()
--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Group;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $groups = Group::orderBy('name')->paginate(10);
+        return // ToDo : View for this shit
+    }
 }
